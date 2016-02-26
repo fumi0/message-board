19ab07b30cfca2602923afd0330c744433913f39
fumi0 <fu-m1110@i.softbank.jp>
2016-02-26 04:24:38 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    # @message = Message.find(1)
+    @messages = Message.all
   end
+
+  def create
+    # params[:message][:id] = 1
+    # puts "!!!!!!!!!!!!"
+    # puts params
+    # puts message_params
+    # puts "!!!!!!!!!!!!"
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  ## ここまで
 end
